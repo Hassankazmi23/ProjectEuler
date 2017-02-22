1ba9b2369c07b51a2d8f5c2a2a13817b5ca9bdc1
Hassankazmi23 <Hassankazmi23@live.com>
2017-02-22 16:00:14 -0500

    Initial import of java project

---
+++ Documents/workspace/PE12/src/PE12.java
@@ -0,0 +1,25 @@
+public class PE12 {
+    public static void main (String[] args) {
+        int i = 1;
+        int x = 2;
+        boolean found = false;
+        while (!found) {
+            if (divisors(i) > 250) {
+                System.out.println(i);
+                found = true;
+            }
+            else {
+                i +=x;
+                x++;
+            }
+        }
+    }
+
+    public static int divisors (int n) {
+        int counter = 0;
+        for (int i = 1; i*i <= n; i++) {
+            if (n % i == 0) counter++;
+        }
+        return counter;
+    }
+}
